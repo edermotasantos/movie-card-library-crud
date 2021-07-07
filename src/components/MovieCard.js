47eadcfef9ada161e94e04228263d35d1df94b85
Eder Santos <3derms@gmail.com>
2021-07-07 12:59:06 -0300

    Realiza uma requisição para buscar o filme que será editado em EditMovie

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 /**
  * Consultei o repositório do Guilherme Oliveira para resolver essa parte.
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+
+/**
+ * Consultei o repositório do Eduardo Texeira para resolver essa parte.
+ * Link: https://github.com/tryber/sd-011-project-movie-card-library-crud/pull/158/files
+ */
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      status: 'loading',
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleSubmit(updatedMovie) {
-  // }
+  componentDidMount() {
+    this.reqMovie();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  async reqMovie(){
+    const { match } = this.props;
+    const { id } = match.params;
+    const foundMovie = await movieAPI.getMovie(id);
+    this.setState({
+      movie: foundMovie,
+      status: false,
+    });
+  }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
+    if (shouldRedirect) return <Redirect to="/" />;
+    if (status === 'loading') return <Loading />;
 
     return (
       <div data-testid="edit-movie">
@@ -30,5 +54,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
