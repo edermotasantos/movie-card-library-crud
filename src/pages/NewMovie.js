aebf27e91318bb7dc8693ac490a1cba8f9b2f39a
Eder Santos <3derms@gmail.com>
2021-07-07 15:31:49 -0300

    Altera nome de variavel em NewMovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -3,26 +3,31 @@ import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
+/**
+ * Consultei o repositório do Eduardo Texeira para resolver essa parte.
+ * Link: https://github.com/tryber/sd-011-project-movie-card-library-crud/pull/158/files
+ */
+
 class NewMovie extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      render: false,
+      shouldRedirect: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async handleSubmit(newMovie) {
     await movieAPI.createMovie(newMovie);
-    this.setState({ render: true });
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
-    const { render } = this.state;
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
-        { render && <Redirect to="/" /> }
         <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
